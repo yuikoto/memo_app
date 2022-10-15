dd57b05b5694e7d59ca364c8849f0f255dd36462
yuikoto <soraifu.com@gmail.com>
2022-10-15 15:04:35 +0900

    更新・削除機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -39,4 +39,34 @@ class MemoController extends Controller
         $memo = Memo::find($id);
         return view('memos.show', ['memo' => $memo]);
     }
+
+    public function edit($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.edit', ['memo' => $memo]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $memo = Memo::find($id);
+
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        // 保存
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect('/memos');
+    }
+
+    public function destroy($id)
+    {
+        $memo = Memo::find($id);
+        $memo->delete();
+
+        return redirect('/memos');
+    }
 }
