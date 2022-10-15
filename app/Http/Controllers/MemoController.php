a7bdb3b1f866939fb3dd88da090c8c7a9d8fcda4
yuikoto <soraifu.com@gmail.com>
2022-10-15 11:38:54 +0900

    照会機能作成

---
+++ app/Http/Controllers/MemoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Memo;
+use Illuminate\Http\Request;
+
+
+class MemoController extends Controller
+{
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
+}
