4f57edc5009a2186f0e07443871e70bb66121ba9
yuikoto <soraifu.com@gmail.com>
2022-10-15 14:10:24 +0900

    登録機能作成

---
+++ resources/views/memos/create.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>memo create</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <a href="/memos">戻る</a>
+    <h1>新規登録</h1>
+    <form action="/memos" method="post">
+    @csrf
+        <p>
+            <label for="title">タイトル</label><br>
+            <input type="text" name="title">
+        </p>
+        <p>
+            <label for="body">本文</label><br>
+            <textarea name="body" class="body"></textarea>
+        </p>
+
+        <input type="submit" value="登録">
+    </form>
+</body>
+</html>
---
+++ resources/views/memos/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <h1>タイトル一覧</h1>
+    <ul>
+        @foreach ($memos as $memo)
+            <!-- // リンク先をidで取得し名前で出力 -->
+            <li><a href="/memos/{{ $memo->id }}">{{ $memo->title }}</a></li>
+        @endforeach
+    </ul>
+    <!-- 新規登録画面へジャンプする -->
+    <button onclick="location.href='/memos/create'">登録する</button>
+</body>
+</html>
