b7d8ff2e812afe4b6b5774a1924b4b97b29dd852
yuikoto <soraifu.com@gmail.com>
2022-10-15 16:14:25 +0900

    バリデーション実装

--- resources/views/memos/create.blade.php
+++ resources/views/memos/create.blade.php
@@ -10,15 +10,29 @@
 <body>
     <a href="/memos">戻る</a>
     <h1>新規登録</h1>
+    
+    @if ($errors->any())
+        <div class="error">
+            <p>
+                <b>{{ count($errors) }}件のエラーがあります。</b>
+            </p>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="/memos" method="post">
-    @csrf
+        @csrf
         <p>
             <label for="title">タイトル</label><br>
-            <input type="text" name="title">
+            <input type="text" name="title" value="{{ old('title') }}">
         </p>
         <p>
             <label for="body">本文</label><br>
-            <textarea name="body" class="body"></textarea>
+            <textarea name="body" class="body">{{ old('body') }}</textarea>
         </p>
 
         <input type="submit" value="登録">
--- resources/views/memos/edit.blade.php
+++ resources/views/memos/edit.blade.php
@@ -10,21 +10,34 @@
 <body>
     <a href="/memos/{{ $memo->id }}">戻る</a>
     <h1>更新</h1>
+
+    @if ($errors->any())
+        <div class="error">
+            <p>
+                <b>{{ count($errors) }}件のエラーがあります。</b>
+            </p>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <!-- 更新先はmemosのidにしないと増える php artisan rote:listで確認① -->
     <form action="/memos/{{ $memo->id }}" method="post">
         @csrf
         @method('PATCH')
         <p>
             <label for="title">タイトル</label><br>
-            <input type="text" name="title" value="{{ $memo->title }}">
+            <input type="text" name="title" value="{{ old('title', $memo->title) }}">
         </p>
         <p>
             <label for="body">本文</label><br>
-            <textarea name="body" class="body">{{ $memo->body }}</textarea>
+            <textarea name="body" class="body">{{ old('body', $memo->body) }}</textarea>
         </p>
 
         <input type="submit" value="更新">
     </form>
 </body>
 </html>
-
